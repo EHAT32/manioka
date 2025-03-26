06e745aa73033913023280067f0de54e06213c7c
Roman Kozin <enatik14@gmail.com>
2025-03-26 16:56:11 +0300

    some tweaks

--- train.py
+++ train.py
@@ -1,30 +1,45 @@
 from model import RootVolumeNet
 from dataset import RootVolumeDataset
+import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+
+def find_max_slice(csv_path):
+    df = pd.read_csv(csv_path)
+    return np.max(df["End"] - df["Start"]) + 1
+
+def get_params():
+    size_file = open("target_size.txt", "r")
+    width, height = [int(line.replace("\n", "").split(" ")[-1]) for line in size_file.readlines()]
+    params = {}
+    params["width"] = width
+    params["heigth"] = height
+    params["max_slices"] = find_max_slice("Train.csv")
+    return params
 
 def train():
     # Hyperparameters
     BATCH_SIZE = 8
     LEARNING_RATE = 1e-4
     NUM_EPOCHS = 50
-    
+    params = get_params()
     # Dataset
     dataset = RootVolumeDataset(
-        csv_path='data.csv',
-        img_root='images/',
-        target_width=2048,
-        target_height=16
+        csv_path='Train.csv',
+        img_root='images/train/',
+        target_width=params["width"],
+        target_height=params["height"]
     )
     
     # DataLoader
     dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # Model
-    model = RootVolumeNet(max_slices=50).to(device)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
-    criterion = nn.L1Loss()
+    model = RootVolumeNet(max_slices=params["max_slices"]).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    criterion = nn.MSELoss()
     
     # Training loop
     for epoch in range(NUM_EPOCHS):
